82a0c7c9738c7221e3b3dfde956921693e311e16
Xen-thus <Xen-thus@users.noreply.github.com>
2024-09-26 19:52:44 +0100

    Added player, moved to group system

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     pygame.init()
+    running = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    while True:
+    while running:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for sprite in updateable:
+            sprite.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
